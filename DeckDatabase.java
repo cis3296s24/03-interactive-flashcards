27ba99f5789ebfa2d401d29c06fe534dd708baab
fbassill <tun39698@temple.edu>
2024-04-02 17:41:39 -0400

    Added different JSON lib, added read() and read_json() methods

--- DeckDatabase.java
+++ DeckDatabase.java
@@ -1,17 +1,18 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class DeckDatabase {
-
     public DeckDatabase(){}
 
     public void write(Deck deck) {
-        try (FileWriter file = new FileWriter(deck.deck_name + ".json")) {
+        try (FileWriter file = new FileWriter("json_decks/" + deck.deck_name + ".json")) {
             file.write(get_json(deck).toString());
             file.close();
         } catch (IOException e) {
@@ -19,15 +20,50 @@ public class DeckDatabase {
         }
     }
 
+    public ArrayList<Deck> read() {
+        ArrayList<Deck> decks = new ArrayList<Deck>();
+        File json_decks_dir = new File("/json_decks");
 
-    private JSONArray get_json(Deck deck) {
-        JSONArray json_deck = new JSONArray();
+        if (json_decks_dir.exists() && json_decks_dir.isDirectory()) {
+            File[] files = json_decks_dir.listFiles();
+
+            if (files!= null) {
+                for (File file : files) {
+                    if (file.isFile() && file.getName().endsWith(".json")) {
+                        try {
+                            FileReader reader = new FileReader(file);
+                            decks.add(read_json(new JSONObject(reader), file.getName().split(".")[0]));
+                        } catch(IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return decks;
+    }
+
+
+    private JSONObject get_json(Deck deck) {
+        JSONObject json_deck = new JSONObject();
         for (int i = 0; i < deck.size(); i++) {
-            JSONObject obj = new JSONObject();
-            obj.put(deck.get(i).question, deck.get(i).answer);
-            json_deck.add(obj);
+            json_deck.put(deck.get(i).question, deck.get(i).answer);
         }
         return json_deck;
+    }
+
+    private Deck read_json(JSONObject obj, String name) {
+        Deck deck = new Deck(name);
+        Iterator<String> keys = obj.keys();
+
+        while(keys.hasNext()) {
+            String key = keys.next();
+            FlashCard card = new FlashCard();
+            card.question = key;
+            card.answer = obj.get(key).toString();
+            deck.add(card);
+        }
+        return deck;  
     }
 
 
