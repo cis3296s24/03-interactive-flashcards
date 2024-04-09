15c0d96be950ecf5aa8149da4539e75a2aab2d56
fbassill <tun39698@temple.edu>
2024-04-09 14:45:31 -0400

    Create card buttons in grid layout.

--- DeckMenu.java
+++ DeckMenu.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class DeckMenu {
     private JFrame frame;
     private ArrayList<Deck> decks;
     private DeckDatabase database = new DeckDatabase();
+    private JPanel cardGrid = new JPanel();
+
     public DeckMenu() {
         frame = new JFrame("Flash Card");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,13 +29,57 @@ public class DeckMenu {
         JComboBox<String> dropdown = new JComboBox<>(deckNames);
         JLabel dropdownLabel = new JLabel("Decks");
 
+        cardGrid.setLayout(new GridLayout((int)Math.ceil(decks.size()/4),4));
+
         mainPanel.add(dropdownLabel);
         mainPanel.add(dropdown);
 
-
-        frame.getContentPane().add(BorderLayout.CENTER,mainPanel);
+        frame.getContentPane().add(BorderLayout.NORTH,mainPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, cardGrid);
         frame.setSize(450,600);
         frame.setVisible(true);
+
+        dropdown.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox<String> combo = (JComboBox<String>) e.getSource();
+                String selected = (String) combo.getSelectedItem();
+                Deck deck;
+                if (selected == "Select Deck...") {return;}
+                else if (selected == "+ Create New Deck") {return;}
+                else {
+                    displayCards(getDeck(selected));
+                }
+            }
+        });
+    }
+
+    private Deck getDeck(String name) {
+        for (Deck deck : decks) {
+            if (deck.deck_name == name) {
+                return deck;
+            }
+        }
+        return null;
+    }
+
+    private void displayCards(Deck deck){
+        clearCardGrid();
+        for (int i = 0;i < deck.size();i++) {
+            FlashCard card = deck.get(i);
+            JButton cardButton = new JButton(card.question);
+            cardButton.setMaximumSize(new Dimension(Integer.MAX_VALUE,20));
+            cardGrid.add(cardButton);
+
+        }
+        frame.revalidate();
+        frame.repaint();
+    }
+
+    private void clearCardGrid() {
+        cardGrid.removeAll();
+        frame.revalidate(); // Update layout
+        frame.repaint(); // Redraw components
     }
 
     public static void main(String[] args) {
