be046c6b18742f26aa7e4e20f0fcf64f7b715216
Alyssa Richner <tui98459@temple.edu>
2024-04-12 02:14:20 -0400

    cleaned displaySettingsMenu

--- DisplaySettingsMenu.java
+++ DisplaySettingsMenu.java
@@ -24,57 +24,8 @@ public class DisplaySettingsMenu{
         // create frame
         DSFrame = new JFrame("Display Settings");
         DSFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-    //try
-        //JPanel DSPanel = new JPanel();
-
-                // format text
-       /*  Border blackBorder = BorderFactory.createLineBorder(black);
-        DSPanel.setBorder(blackBorder);
-        DSPanel.setMinimumSize(new Dimension(430,250)); 
-        DSPanel.setPreferredSize(new Dimension(430,250));
-        DSPanel.setMaximumSize(new Dimension(430,250));*/
-
-        /*JButton b1 = new JButton("Button 1");
-        DSPanel.add(b1); 
-        DSFrame.add(DSPanel);  */
-
-    //try
-        /*DSMenu = new JPopupMenu("this is a menu"); 
-        
-        JMenuItem menuItem1 = new JMenuItem("this is an item"); 
-        DSMenu.add(menuItem1); 
-        JMenuItem menuItem2 = new JMenuItem("item 2"); 
-        DSMenu.add(menuItem2); 
-        
-
-        DSMenu.setSize(400,400);
-        DSMenu.setVisible(true);
-        
-        DSFrame.add(DSMenu);*/
-
-
-
-
-
-        /*CHANGE FONT COMBOBOX */   //moved to method = DELETE
-
-        /* gets all fonts in system and puts them into fontStrings list */
-        /*GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-                
-        Font []getFonts = ge.getAllFonts();    //returns Font array of all fonts available in graphical interface
-        String []fontStrings = new String[getFonts.length]; //creates String array for font names to go into 
-
-        //gets names of all fonts and puts into fontStrings array
-            for(int i = 0; i<getFonts.length; i++){ //prints all fonts to output
-                //System.out.print(getFonts[i].getName() + "\n"); //prints all fonts to screen DELETE (eventually)
-                fontStrings[i] = getFonts[i].getName(); 
-            }*/
-
-
 
         createFontStringArr(); 
-
 
         /*create comboBox fontList */
         JComboBox <String> fontList = new JComboBox<>(fontStrings); //creates fontList combobox
@@ -101,15 +52,16 @@ public class DisplaySettingsMenu{
 
         /* change font size */
 
-
         DSFrame.add(displaySettingsPanel);  //add comboBox fontLIst to frame
         DSFrame.setSize(600,600);   // format frame
         DSFrame.setVisible(true);   //set visible    
     }
     
+    
     public String getNewFont(){
         return newText; 
     }
+
 
     /* gets all fonts in system and puts them into fontStrings list */
     public static void createFontStringArr(){
